f3c626ecf9f636814aa64dfd8a5eebf152ca6959
HantikMine <vadim_dr@hotmail.com>
2022-12-26 18:01:04 +0300

    1

--- Введение в JavaScript/1) Возведение в степень/pow.js
+++ Введение в JavaScript/1) Возведение в степень/pow.js
@@ -2,7 +2,8 @@ function pow(x, n) {
 	if(n === 0) return 1;
 	if(x === 0) return 0;
 	if(x === 1) return 1;
-
+	if(n % 1 !== 0 || n < 0) return "Число должно быть натуральным";
+    
 	let result = 1;
 	while(n !== 0) {
 		n -= 1;
